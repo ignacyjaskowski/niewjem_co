ab6206154595ff28f06483a1c0927bd31f99aba0
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-25 17:32:55 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
-})
+szukaj([5, 2, 9, 7, 1, 12, 98, 34, 77, 9, 1111], 1111)
+function szukaj(arr: number[], k: number) {
+    for (let i = 0; i <= 11; i++) {
+        if (arr[i] == k) {
+            for (let index = 0; index < 10; index++) {
+                basic.showLeds(`
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    # # # # #
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+                basic.pause(100)
+            }
+        }
+        basic.pause(100)
+    }
+}
